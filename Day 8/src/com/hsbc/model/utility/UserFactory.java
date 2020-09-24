a1450c018d9a1a965527f4a208bbe469edc1f7d8
Abhishek Nayyar <abhi.nayyar14@gmail.com>
2020-09-24 10:39:00 +0530

    Day 8 Assignment - FileBacked Updated

---
+++ Day 8/src/com/hsbc/model/utility/UserFactory.java
@@ -0,0 +1,23 @@
+package com.hsbc.model.utility;
+
+import com.hsbc.model.business.UserServiceImpl;
+import com.hsbc.model.dao.FileBackedUserDao;
+
+
+public class UserFactory {
+	// it can return dao, service or controller instance
+	
+	
+	public static Object getInstance(Type type) {
+		Object obj = null;
+		switch(type) {
+		case DAO : 
+			obj = new FileBackedUserDao();
+			break;
+		case SERVICE :
+			obj = new UserServiceImpl();
+			break;
+		}
+		return obj;
+	}
+}
